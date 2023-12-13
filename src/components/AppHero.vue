f4eaef312764b47dc4f149bce109c4d7948860ea
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-12-13 15:14:17 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+        
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <h1>This is a Hero Section</h1>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        height: 500px;
+        width: 800px;
+        background-color: rgb(173, 85, 173);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* color: white; */
+    }
+    div h1{
+        color: white;
+        
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        title: 'asass',
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
